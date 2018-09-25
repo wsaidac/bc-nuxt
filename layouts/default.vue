a715718302702e591a1cdb6f047c0c112698d24b
Roland Boon <roland@rolandboon.com>
2018-09-25 17:56:32 +0200

    feat: init dropdown navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <header-navbar />
+    <header-navbar :items="items" />
     <nuxt />
     <rapido-footer />
   </div>
@@ -14,6 +14,115 @@ export default {
   components: {
     HeaderNavbar,
     RapidoFooter,
+  },
+
+  data() {
+    return {
+      items: [
+        {
+          title: 'Mobile recharge',
+          icon: 'mobile',
+          products: [
+            {
+              id: 5,
+              title: 'Verizon',
+              imageUrl: 'https://static.rapido.com/media/topup/rapido/default/images/most-popular/verizon.png',
+              url: '/verizon',
+            },
+            {
+              id: 4,
+              title: 'T-Mobile',
+              imageUrl: 'https://static.rapido.com/media/topup/rapido/default/images/most-popular/t-mobile.png',
+              url: '/t-mobile',
+            },
+            {
+              id: 6,
+              title: 'AT&T',
+              imageUrl: 'https://static.rapido.com/media/topup/rapido/default/images/most-popular/atandt.png',
+              url: '/atandt',
+            },
+            {
+              id: 7,
+              title: 'Verizon',
+              imageUrl: 'https://static.rapido.com/media/topup/rapido/default/images/most-popular/verizon.png',
+              url: '/verizon',
+            },
+            {
+              id: 8,
+              title: 'T-Mobile',
+              imageUrl: 'https://static.rapido.com/media/topup/rapido/default/images/most-popular/t-mobile.png',
+              url: '/t-mobile',
+            },
+            {
+              id: 9,
+              title: 'AT&T',
+              imageUrl: 'https://static.rapido.com/media/topup/rapido/default/images/most-popular/atandt.png',
+              url: '/atandt',
+            },
+            {
+              id: 5,
+              title: 'Verizon',
+              imageUrl: 'https://static.rapido.com/media/topup/rapido/default/images/most-popular/verizon.png',
+              url: '/verizon',
+            },
+            {
+              id: 10,
+              title: 'T-Mobile',
+              imageUrl: 'https://static.rapido.com/media/topup/rapido/default/images/most-popular/t-mobile.png',
+              url: '/t-mobile',
+            },
+            {
+              id: 11,
+              title: 'AT&T',
+              imageUrl: 'https://static.rapido.com/media/topup/rapido/default/images/most-popular/atandt.png',
+              url: '/atandt',
+            },
+            {
+              id: 12,
+              title: 'Verizon',
+              imageUrl: 'https://static.rapido.com/media/topup/rapido/default/images/most-popular/verizon.png',
+              url: '/verizon',
+            },
+            {
+              id: 13,
+              title: 'T-Mobile',
+              imageUrl: 'https://static.rapido.com/media/topup/rapido/default/images/most-popular/t-mobile.png',
+              url: '/t-mobile',
+            },
+            {
+              id: 14,
+              title: 'AT&T',
+              imageUrl: 'https://static.rapido.com/media/topup/rapido/default/images/most-popular/atandt.png',
+              url: '/atandt',
+            },
+            {
+              id: 15,
+              title: 'T-Mobile',
+              imageUrl: 'https://static.rapido.com/media/topup/rapido/default/images/most-popular/t-mobile.png',
+              url: '/t-mobile',
+            },
+            {
+              id: 16,
+              title: 'AT&T',
+              imageUrl: 'https://static.rapido.com/media/topup/rapido/default/images/most-popular/atandt.png',
+              url: '/atandt',
+            },
+          ],
+        },
+        {
+          title: 'Entertainment Gift Cards',
+          icon: 'mobile',
+          products: [
+            {
+              id: 3,
+              title: 'Spotify',
+              imageUrl: 'https://static.rapido.com/media/topup/rapido/default/images/most-popular/spotify-gift-card.png',
+              url: '/spotify',
+            },
+          ],
+        },
+      ],
+    };
   },
 };
 </script>
