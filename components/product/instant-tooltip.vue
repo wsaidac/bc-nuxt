991623a0a7d9cee1f3cdd9b4c2a6c024901450ac
Roland Boon <roland@rolandboon.com>
2018-09-25 15:08:12 +0200

    feat: instant delivery tooltip

--- components/product/instant-tooltip.vue
+++ components/product/instant-tooltip.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="product-instant-tooltip">
-    <p>Instant delivery</p>
+    <ui-popover
+      title="Instant delivery"
+      content="You'll receive the code by email, so you can use the credit right away. In most cases your code will be displayed instantly on your screen as well."
+      placement="bottom-start"
+      trigger="hover"
+    >
+      <p slot="reference">
+        <span>Instant delivery</span>
+        <ui-icon icon="question-circle-o" />
+      </p>
+    </ui-popover>
   </div>
 </template>
 
 <script>
+import { UiIcon, UiPopover } from '~/components/ui';
+
 export default {
   name: 'ProductInstantTooltip',
+
+  components: {
+    UiIcon,
+    UiPopover,
+  },
 };
 </script>
 
@@ -14,6 +31,7 @@ export default {
 .product-instant-tooltip {
   p {
     color: $primary-500;
+    font-size: $font-size-h6;
   }
 }
 </style>
