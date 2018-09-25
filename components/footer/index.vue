38c4acc20bf161aa47185322c4ad2b56b8847304
Roland Boon <roland@rolandboon.com>
2018-09-25 17:11:57 +0200

    feat: footer components

---
+++ components/footer/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <footer class="rapido-footer">
+    <hr>
+    <div class="container">
+      <ui-row>
+        <ui-col :sm="12">
+          <footer-payment-methods
+            :methods="methods"
+          />
+        </ui-col>
+      </ui-row>
+    </div>
+    <hr>
+    <footer-links
+      :columns="links"
+    />
+    <footer-copyright/>
+  </footer>
+</template>
+
+<script>
+import { UiRow, UiCol } from '~/components/ui';
+import FooterPaymentMethods from './payment-methods';
+import FooterLinks from './links';
+import FooterCopyright from './copyright';
+
+export default {
+  name: 'RapidoFooter',
+
+  components: {
+    UiRow,
+    UiCol,
+    FooterPaymentMethods,
+    FooterLinks,
+    FooterCopyright,
+  },
+
+  data() {
+    return {
+      methods: [
+        { name: 'Visa', imageUrl: 'https://static.rapido.com/media/topup/shop/images/paymethods/visa.png' },
+        { name: 'Mastercard', imageUrl: 'https://static.rapido.com/media/topup/shop/images/paymethods/mastercard.png' },
+        { name: 'Maestro', imageUrl: 'https://static.rapido.com/media/topup/shop/images/paymethods/maestro.png' },
+        { name: 'American Express', imageUrl: 'https://static.rapido.com/media/topup/shop/images/paymethods/american-express.png' },
+        { name: 'PayPal', imageUrl: 'https://static.rapido.com/media/topup/shop/images/paymethods/pay-pal.png' },
+      ],
+
+      links: [
+        {
+          title: 'Rapido.com',
+          links: [
+            { title: 'About us', url: '/about-us' },
+            { title: 'Payment options', url: '/payment-options' },
+          ],
+        },
+        {
+          title: 'Customer Care',
+          links: [
+            { title: 'Help', url: '/help' },
+          ],
+        },
+        {
+          title: 'Terms of Use',
+          links: [
+            { title: 'Privacy Policy', url: '/privacy-policy' },
+            { title: 'Conditions', url: '/conditions' },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.rapido-footer {
+  hr {
+    background: $gray-400;
+    border: none;
+    color: $gray-400;
+    height: 1px;
+  }
+}
+</style>
---
+++ components/footer/links.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="footer-links container">
+    <div
+      v-for="column in columns"
+      :key="column.title"
+      class="footer-links__category"
+    >
+      <h5 v-text="column.title" />
+      <ul class="list-unstyled">
+        <li
+          v-for="link in column.links"
+          :key="link.title"
+        >
+          <nuxt-link
+            :to="link.url"
+            :title="link.title"
+          >
+            {{ link.title }}
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterLinks',
+
+  props: {
+    columns: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.footer-links {
+  align-items: flex-start;
+  display: flex;
+
+  @include media-breakpoint-up('sm') {
+    margin-bottom: 40px;
+    margin-top: 40px;
+  }
+
+  &__category {
+    flex: 1 1;
+    margin: 0;
+    text-align: center;
+
+    h5 {
+      margin: 0;
+    }
+
+    a {
+      color: $body-color;
+    }
+  }
+}
+</style>
