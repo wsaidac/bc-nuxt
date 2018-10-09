f9bb5095c9171328ac2bddeaec23c49def99ac72
Roland Boon <roland@rolandboon.com>
2018-10-09 16:55:27 +0200

    fix: refactor and split customer service component

--- components/footer/index.vue
+++ components/footer/index.vue
@@ -1,7 +1,12 @@
 <template>
   <footer class="rapido-footer">
     <div class="container">
-      <footer-help />
+      <footer-customer-service
+        :link="customerService.link"
+        :image="customerService.image"
+        :title="customerService.primaryText"
+        :description="customerService.secondaryText"
+      />
     </div>
     <hr>
     <div class="container">
@@ -25,9 +30,10 @@
 import { mapGetters } from 'vuex';
 import { UiRow, UiCol } from '~/components/ui';
 import FooterPaymentMethods from './payment-methods';
+import FooterCustomerService from './customer-service';
+import FooterSeoBlock from './seo-block';
 import FooterLinks from './links';
 import FooterCopyright from './copyright';
-import FooterHelp from './help';
 
 export default {
   name: 'RapidoFooter',
@@ -36,21 +42,14 @@ export default {
     UiRow,
     UiCol,
     FooterPaymentMethods,
+    FooterCustomerService,
+    FooterSeoBlock,
     FooterLinks,
     FooterCopyright,
-    FooterHelp,
   },
 
   data() {
     return {
-      methods: [
-        { name: 'Visa', imageUrl: 'https://static.rapido.com/media/topup/shop/images/paymethods/visa.png' },
-        { name: 'Mastercard', imageUrl: 'https://static.rapido.com/media/topup/shop/images/paymethods/mastercard.png' },
-        { name: 'Maestro', imageUrl: 'https://static.rapido.com/media/topup/shop/images/paymethods/maestro.png' },
-        { name: 'American Express', imageUrl: 'https://static.rapido.com/media/topup/shop/images/paymethods/american-express.png' },
-        { name: 'PayPal', imageUrl: 'https://static.rapido.com/media/topup/shop/images/paymethods/pay-pal.png' },
-      ],
-
       links: [
         {
           title: 'Rapido.com',
---
+++ components/footer/seo-block.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="footer-seo-block">
+    <h2 v-text="customerService.closingTitle"/>
+    <p v-text="customerService.closingText"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterBlockText',
+
+  props: {
+  },
+};
+</script>
+
+<style lang="scss">
+.footer-seo-block {
+}
+</style>
