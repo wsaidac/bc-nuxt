5a18353ccb6cab411c0686620fdb55ec5462b998
Roland Boon <roland@rolandboon.com>
2018-10-09 17:12:00 +0200

    feat: seo block component

--- components/footer/seo-block.vue
+++ components/footer/seo-block.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="footer-seo-block">
-    <h2 v-text="customerService.closingTitle"/>
-    <p v-text="customerService.closingText"/>
+    <h2 v-text="title" />
+    <p v-text="description" />
   </div>
 </template>
 
@@ -10,11 +10,31 @@ export default {
   name: 'FooterBlockText',
 
   props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
   },
 };
 </script>
 
 <style lang="scss">
 .footer-seo-block {
+  @include media-breakpoint-up('md') {
+    padding: 20px 0 40px;
+  }
+
+  h2 {
+    font-size: $font-size-h5;
+    margin: 0;
+  }
+
+  p {
+    margin: 0;
+  }
 }
 </style>
