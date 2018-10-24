66b99e1562bf0f19478ba21759ac3ba49620f362
Roland Boon <roland@rolandboon.com>
2018-10-24 18:12:14 +0200

    RAP-1282 integrate detail page

--- pages/product.vue
+++ pages/product.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="cg-product">
     <header-banner
-      :image="header.image"
-      :payoff-image="header.payoffImage"
+      :image="category.categoryHeader.banner"
+      :payoff-image="category.categoryHeader.image"
+      :title="category.categoryHeader.title"
     />
     <cg-usps
       :usps="usps.items"
     />
     <service-title
-      :title="highlights.title"
+      :title="post.content.title"
       tooltip-message="Instant delivery"
     />
     <div class="block block--gray">
@@ -21,7 +22,7 @@
             :xs="24"
           >
             <product-card
-              :product="product"
+              :product="post"
               has-select
             />
           </ui-col>
@@ -32,13 +33,13 @@
             :xs="24"
           >
             <category-highlights
-              :title="highlights.title"
-              :description="highlights.description"
+              :title="category.highlight.question"
+              :description="category.highlight.answer"
             />
-            <product-variants
+            <!-- <product-variants
               :product="product"
               :variants="relatedProducts"
-            />
+            /> -->
           </ui-col>
         </ui-row>
       </div>
@@ -49,14 +50,14 @@
         <ui-col :sm="12">
           <category-accordion
             :usps="usps.items"
-            :slides="infoSlides"
+            :slides="category.faqQuestions.questions"
           />
         </ui-col>
         <ui-col :sm="12">
           <div class="block block--blue block--padded">
             <seo-block
-              :title="seoBlock.title"
-              :description="seoBlock.description"
+              :title="category.infoBlock.title"
+              :description="category.infoBlock.text"
             />
           </div>
         </ui-col>
@@ -66,8 +67,8 @@
     <div class="container container--mobile-padded">
       <service-button />
       <service-terms
-        :title="serviceTerms.title"
-        :description="serviceTerms.description"
+        :title="category.terms.title"
+        :description="category.terms.text"
       />
       <service-banner
         :link="customerService.link"
@@ -100,90 +101,6 @@ import ProductCard from '~/components/product/card';
 import ServiceTitle from '~/components/service/title';
 import ProductVariants from '~/components/product/variants';
 
-const header = {
-  image: {
-    desktop: 'http://localhost:3000/app/uploads/seeds/rapido_header.jpg',
-    mobile: 'http://localhost:3000/app/uploads/seeds/rapido_header.jpg',
-  },
-  payoffImage: {
-    regular: 'http://localhost:3000/app/uploads/seeds/rapido_header.jpg',
-    retina: 'http://localhost:3000/app/uploads/seeds/rapido_header.jpg',
-  },
-};
-
-const infoSlides = [
-  {
-    title: 'What can i use my Xbox Gift card for?',
-    content: 'You can use your Gift Card to buy Xbox games and downloadable content, movies, TV shows, music, apps and bundles. Once you’ve redeemed your Xbox Gift Card using your Microsoft account, you can spend your balance on your Xbox, through Windows and at the Microsoft Store online', // eslint-disable-line
-  },
-  {
-    title: 'What kind of account do I need to redeem my Xbox Gift Card?',
-    content: 'You can use your Gift Card to buy Xbox games and downloadable content, movies, TV shows, music, apps and bundles. Once you’ve redeemed your Xbox Gift Card using your Microsoft account, you can spend your balance on your Xbox, through Windows and at the Microsoft Store online', // eslint-disable-line
-  },
-  {
-    title: 'What can i use my Xbox Gift card for?',
-    content: 'You can use your Gift Card to buy Xbox games and downloadable content, movies, TV shows, music, apps and bundles. Once you’ve redeemed your Xbox Gift Card using your Microsoft account, you can spend your balance on your Xbox, through Windows and at the Microsoft Store online', // eslint-disable-line
-  },
-  {
-    title: 'What can i use my Xbox Gift card for?',
-    content: 'You can use your Gift Card to buy Xbox games and downloadable content, movies, TV shows, music, apps and bundles. Once you’ve redeemed your Xbox Gift Card using your Microsoft account, you can spend your balance on your Xbox, through Windows and at the Microsoft Store online', // eslint-disable-line
-  },
-  {
-    title: 'What can i use my Xbox Gift card for?',
-    content: 'You can use your Gift Card to buy Xbox games and downloadable content, movies, TV shows, music, apps and bundles. Once you’ve redeemed your Xbox Gift Card using your Microsoft account, you can spend your balance on your Xbox, through Windows and at the Microsoft Store online', // eslint-disable-line
-  },
-];
-
-const highlights = {
-  title: 'EasyGO Refill',
-  description: `
-  <h5>What am i buying?</h5>
-  <p>These EasyGO Refills allow you to recharge/refill the amount of credits on your EasyGO mobile account.</p>
-  <h5>What am i buying?</h5>
-  <p>These EasyGO Refills allow you to recharge/refill the amount of credits on your EasyGO mobile account.</p>
-  <h5>What am i buying?</h5>
-  <p>These EasyGO Refills allow you to recharge/refill the amount of credits on your EasyGO mobile account.</p>
-  <h5>What am i buying?</h5>
-  <p>These EasyGO Refills allow you to recharge/refill the amount of credits on your EasyGO mobile account.</p>
-  `,
-};
-
-const seoBlock = {
-  title: 'Easy Refill EasyGO',
-  description: 'On Rapido.com you can easily buy an easyGO refill voucher. Receive your EasyGO recharge code within 30 seconds! All you need to do is select one of the easyGO mobile plans, submit your email address, choose one of our secure payment options and proceed to the checkout. We’ll send you the easyGO recharge voucher by email.',
-};
-
-const serviceTerms = {
-  title: 'Terms & Conditions',
-  description: 'By using this service, you consent to the Terms of Service available at www.easygowireless.com. Unlimited does not mean unreasonable use. EasyGO reserves the right to reduce data speeds and/or terminate your service for unauthorized or abnormal usage. Video streaming or other high-bandwidth applications may be available on Wi-Fi only. Many jurisdictions impose recurring taxes and fees that may be debited from your account. No refunds or exchanges. If you change plans prior to your airtime expiration date, you may lose any remaining value on your account.<br><br>Required state and local taxes or surcharges may apply to this purchase.<br><br>This PIN is Non-Refundable.',
-};
-
-const crumbs = [
-  { url: '/', label: 'Home', title: 'Rapido Home is cool' },
-  { url: '/category', label: 'category', title: 'cat' },
-  { url: '/faq', label: 'FAQ', title: 'faq' },
-  { url: '/category', label: 'category', title: 'cat' },
-  { label: 'prod', title: 'prod' },
-];
-
-const product = {
-  id: 1,
-  title: 'Verizon Prepaid Refill $5',
-  price: {
-    amount: 5.0,
-    currency: 'USD',
-  },
-  url: '/verizon',
-  imageUrl:
-    'https://static.rapido.com/categories/2023/Zeichenflche_98.png?1532587359',
-};
-
-const relatedProducts = [
-  { id: 2, title: 'Verizon Prepaid Refill $10', url: '/verizon/10-usd' },
-  { id: 3, title: 'Verizon Prepaid Refill $15', url: '/verizon/15-usd' },
-  { id: 4, title: 'Verizon Prepaid Refill $20', url: '/verizon/20-usd' },
-];
-
 export default {
   components: {
     HeaderBanner,
@@ -203,21 +120,27 @@ export default {
     ServiceTerms,
   },
 
-  data() {
-    return {
-      header,
-      infoSlides,
-      highlights,
-      seoBlock,
-      crumbs,
-      product,
-      relatedProducts,
-      serviceTerms,
-    };
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
   },
 
   computed: {
-    ...mapGetters('shared', ['customerService', 'usps', 'paymentMethods']),
+    ...mapGetters('shared', ['customerService', 'usps']),
+
+    category() {
+      return this.post.categories.nodes[0];
+    },
+
+    crumbs() {
+      return [
+        { url: '/', label: 'Home', title: 'Rapido.com: Buy your favorite Gift Cards online | Fast Email Delivery' },
+        { url: this.category.slug, label: this.category.name },
+        { label: this.post.content.title },
+      ];
+    },
   },
 };
 </script>
