214b74c7daf60bb7bac4b5920fbc996080417bcf
ronald <ronald@10kb.nl>
2018-11-16 15:18:56 +0100

    feat: clean up

--- components/product/quickbuy.vue
+++ components/product/quickbuy.vue
@@ -49,7 +49,6 @@ export default {
 
   data() {
     return {
-      timerRunning: false,
       loading: true,
     };
   },
--- store/auth.js
+++ store/auth.js
@@ -9,25 +9,14 @@ export default {
     };
   },
 
-  // getters: {
-  //   context: state => state.context,
-  //   currentUser: state => state.currentUser,
-  //   token: state => state.token,
-  // },
-
   getters: {
-    currentUser: state => {
-      'ronald@10k.nl';
-    },
-    token: state =>
-      'U1dBTkp2cGExMkdpRnhmZk90TkQremFtQmhNMlFwL3JuajMzbWM2dWJidXRyWEJRSVp6a24vVlFKSGY3b09MQ2o2bjZwWlhMd2tpOEh5TVlkcXNlaUZaUkdJOThpVmNRZ05hN0g4Tlc0aytkNGUyWUtjc0psSC9sUFlQclNmNWotLTdWWmd3THpGbmhvZm9DdkVHWDhNSEE9PQ==--4dd4056025d58cef37bf8bd6d10496414d32dd40',
-    context: state => 11,
-    quickbuy: () => null,
+    context: state => state.context,
+    currentUser: state => state.currentUser,
+    token: state => state.token,
   },
 
   mutations: {
     setCurrentUser(state, { token, user, orders }) {
-      console.log('setting current user');
       const extensibleUser = Object.assign({}, user);
       extensibleUser.orders = Object.assign({}, orders);
       state.token = token;
