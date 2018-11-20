0ba3c65ffe7812a65fbb896263a211ce6d5b10c8
ronald <ronald@10kb.nl>
2018-11-20 11:22:04 +0100

    fix: correct function naming

--- components/product/card.vue
+++ components/product/card.vue
@@ -41,7 +41,7 @@
         <a :href="product.slug" >
           <ui-button
             type="warning"
-            @click="pickItem"
+            @click="setAmount"
           >
             {{ cta }}
           </ui-button>
@@ -105,7 +105,7 @@ export default {
   },
 
   methods: {
-    pickItem() {
+    setAmount() {
       this.$store.commit('product/setAmount', this.value);
     },
   },
