02c85692ee0ab57e47dd4f71d3cb235acbf37073
Bauke Palstra <bauke@10kb.nl>
2018-11-21 17:20:50 +0100

    fix: better mock of quickbuy

--- store/auth.js
+++ store/auth.js
@@ -5,10 +5,10 @@ const temporaryDummyQuickbuy = {
     currency: 'USD',
   },
   content: {
-    title: 'Verizon Prepaid Refill $5',
+    title: 'Sporify Gift card $15',
     image: {
-      desktop: 'https://example.com/xbox.jpg',
-      mobile: 'https://example.com/xbox.jpg',
+      desktop: 'https://static.rapido.com/categories/2086/Spotify_Green.png?v=1542612621540',
+      mobile: 'https://static.rapido.com/categories/2086/Spotify_Green.png?v=1542612621540',
     },
     tooltip: {
       content: 'Long text to show when tooltip is hovered',
@@ -20,28 +20,34 @@ const temporaryDummyQuickbuy = {
   categories: {
     nodes: [
       {
-        name: 'easy',
-        slug: '/easy',
+        name: 'Gift Cards',
+        slug: '/spotify',
+        categoryHeader: {
+          image: {
+            regular: 'https://static.rapido.com/categories/2086/Spotify_Green.png?v=1542612621540',
+            retina: 'https://static.rapido.com/categories/2086/Spotify_Green.png?v=1542612621540',
+          },
+        },
         products: {
           nodes: [
             {
               id: 2,
               content: {
-                title: 'Verizon Prepaid Refill $10',
+                title: 'Spotify gift card $10',
               },
               slug: '/verizon/10-usd',
             },
             {
               id: 3,
               content: {
-                title: 'Verizon Prepaid Refill $15',
+                title: 'Spotify gift card $20',
               },
               slug: '/verizon/15-usd',
             },
             {
               id: 4,
               content: {
-                title: 'Verizon Prepaid Refill $20',
+                title: 'Spotify gift card $25',
               },
               slug: '/verizon/20-usd',
             },
