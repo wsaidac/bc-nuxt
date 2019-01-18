4b258551b0c6081aae576d25a792021cd14bd68a
Bauke Palstra <bauke@10kb.nl>
2019-01-18 16:33:22 +0100

    fix: remove second server, not available

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,3 @@
 # frozen_string_literal: true
 
-server 'rapido-frontend-1.eu-west-1.production.cgaws.cloud', user: 'cgdeployer', roles: %w[app web db]
-server 'rapido-frontend-2.eu-west-1.production.cgaws.cloud', user: 'cgdeployer', roles: %w[app web db]
+server 'rapido-frontend-1.eu-west-1.production.cgaws.cloud', user: 'cgdeployer', roles: %w[app web]
