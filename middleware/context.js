0cbdae522ca251ac8c9f42a737f66c04de3ee485
Bauke Palstra <bauke@10kb.nl>
2019-02-13 11:36:09 +0100

    temp: hard code label to rapido

--- middleware/context.js
+++ middleware/context.js
@@ -15,13 +15,13 @@ async function fetchShared(app, store) {
 
 export default ({ req, store, app }) => {
   if (!process.server) return;
-  let label = req.headers.host.split('.').slice(-2)[0];
+  // let label = req.headers.host.split('.').slice(-2)[0];
   const contextSlug = req.url.split('/')[1];
   const [language, country] = contextSlug.split('-');
 
-  if (label.startsWith('localhost')) {
-    label = 'rapido';
-  }
+  // if (label.startsWith('localhost')) {
+  const label = 'rapido';
+  // }
 
   store.commit('context/setCurrentLabel', label);
   store.commit('context/setCurrentCountry', country);
