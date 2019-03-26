09858659279470c3fa8c6d96f5b331df192d5dd1
vpuscoci-cg <valentin.puscoci@cg.nl>
2019-03-26 10:42:54 +0100

    Add blueconic script in head

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,15 @@
 
 const i18nConfig = require('./config/i18nConfig.js');
 
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
+
 
 const conf = {
   head: {
     title: 'rapido_web',
     meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: [{ src: 'https://cdn.blueconic.net/cg.js' }],
   },
   css: ['~/assets/stylesheets/application.scss'],
   store: true,
