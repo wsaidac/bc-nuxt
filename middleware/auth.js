ec09eb2c808f17b2db9c18d56d3bbc8201ded6c2
ronald <ronald@10kb.nl>
2019-04-04 14:39:57 +0200

    feat: change auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,12 +4,12 @@ export default async ({
   const path = process.server ? req.url : window.location.pathname;
   const { locale } = app.i18n;
 
-  if (path.startsWith(`/${locale}/sessions/`)) return;
-
-  const { data, errors } = await app.$query('currentUser');
-  if (errors.length > 0) {
-    redirect(`/${locale}/sessions/login`);
-  } else {
-    store.commit('auth/setCurrentUser', data.user);
+  if (path.startsWith(`/${locale}/account`)) {
+    const { data, errors } = await app.$query('currentUser');
+    if (errors.length > 0) {
+      redirect(`/${locale}/sessions/login`);
+    } else {
+      store.commit('auth/setCurrentUser', data.user);
+    }
   }
 };
