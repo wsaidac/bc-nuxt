84197cc21ef9857d5f383e0067e89a2e4c7780e1
vpuscoci-cg <valentin.puscoci@cg.nl>
2019-04-06 11:20:28 +0200

    refactor: remove unused asyc store state

--- store/async.js
+++ store/async.js
@@ -2,21 +2,16 @@ export default {
   state() {
     return {
       loaded: false,
-      contextChanged: false,
     };
   },
 
   getters: {
     isLoaded: state => state.loaded,
-    hasContextChanged: state => state.contextChanged,
   },
 
   mutations: {
     setLoaded(state, bool) {
       state.loaded = bool;
-    },
-    setContextChange(state, bool) {
-      state.contextChanged = bool;
     },
   },
 };
