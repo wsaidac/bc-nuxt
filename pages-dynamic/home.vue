2b00615523af09ba614ad531ab441882f4c7e8f7
vpuscoci-cg <valentin.puscoci@cg.nl>
2019-04-10 16:00:14 +0200

    feat: add canonical to home

--- pages-dynamic/home.vue
+++ pages-dynamic/home.vue
@@ -62,6 +62,15 @@ export default {
     },
   },
 
+  head() {
+    const url = `https://${this.domain}/${this.$i18n.locale}/`;
+    return {
+      meta: [
+        { rel: 'canonical', href: url },
+      ],
+    };
+  },
+
   computed: {
     ...mapGetters('menus', ['main']),
 
