94bb88d18921464699ec5e71c60f211502b4dddc
vpuscoci-cg <valentin.puscoci@cg.nl>
2019-04-10 16:04:22 +0200

    fix: add domain to canonical meta

--- pages-dynamic/home.vue
+++ pages-dynamic/home.vue
@@ -73,7 +73,7 @@ export default {
 
   computed: {
     ...mapGetters('menus', ['main']),
-
+    ...mapGetters('context', ['domain']),
     ...mapGetters('shared', ['customerService', 'usps']),
   },
 };
