c9c204c6166f8e01cd549a878bc17df4bf445851
ronaldvanderreest <36039050+ronaldvanderreest@users.noreply.github.com>
2019-04-23 14:55:19 +0200

    Fix/rap 1712 redirect (#216)
    * feat: redirect based on cloudfront-viewer-country
    
    * feat: cleanup
    
    * feat: refactor
    
    * feat: check for header present
    
    * refactor
    
    * feat: refactor-2
    
    * feat: refactor, add cookie.get
    
    * remove eslint-disable
    
    * feat: check if CF-header exists
    
    * feat: add initial-cookie
    
    * fix: initial cookie

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -1,6 +1,11 @@
+import Cookies from 'universal-cookie';
+
 export default function ({ app, error }) {
   // onLanguageSwitched called right after a new locale has been set
   app.i18n.onLanguageSwitched = (_, newLocale) => {
+    const cookies = new Cookies();
+    cookies.set('country', newLocale, { path: '/' });
+
     app.store.commit("async/setContextChange", true);
     app.store.dispatch("context/changeContext", { app, error, locale: newLocale });
   };
