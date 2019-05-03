5df9c0f67b5525ac0592e687bf6070bbe177346e
vpuscoci-cg <valentin.puscoci@cg.nl>
2019-05-03 11:17:32 +0200

    fix: use universal cookie for store again, in order to fix the multiple cookie store header being set

--- plugins/cookie-store.js
+++ plugins/cookie-store.js
@@ -1,7 +1,9 @@
+import Cookies from 'universal-cookie';
 import createPersistedState from 'vuex-persistedstate';
 import merge from 'deepmerge';
 
-export default ({ store, app }) => {
+export default ({ store, req }) => {
+  const cookies = new Cookies(process.server && req.headers.cookie);
   const storageKey = 'store';
 
   createPersistedState({
@@ -10,14 +12,14 @@ export default ({ store, app }) => {
     paths: ['auth.token', 'product.amount'],
 
     storage: {
-      getItem: key => app.$cookies.get(key),
-      setItem: (key, value) => app.$cookies.set(key, value, { path: '/' }),
-      removeItem: key => app.$cookies.remove(key),
+      getItem: key => cookies.get(key),
+      setItem: (key, value) => cookies.set(key, value, { path: '/' }),
+      removeItem: key => cookies.remove(key),
     },
   })(store);
 
-  if (process.server && app.$cookies.get(storageKey)) {
-    const savedState = app.$cookies.get(storageKey);
+  if (process.server && cookies.get(storageKey)) {
+    const savedState = cookies.get(storageKey);
     store.replaceState(merge(store.state, savedState, { clone: false }));
   }
 };
