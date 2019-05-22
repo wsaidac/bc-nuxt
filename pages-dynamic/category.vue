003b5d58561e72a6624f27e8d4376e0e33eb2e52
Nick Wagelmans <nick@cg.nl>
2019-05-22 14:29:26 +0200

    Dirty fix for replacing the &amp characters in the brand name. (#307)
    Use rapido product ID instead of post identifier on PDP.

--- pages-dynamic/category.vue
+++ pages-dynamic/category.vue
@@ -103,7 +103,7 @@ export default {
         { property: 'bc:pop:code', content: this.post.id },
         { property: 'bc:pop:slug', content: this.$router.currentRoute.path },
         { property: 'bc:pop:title', content: title },
-        { property: 'bc:brand', content: this.post.name },
+        { property: 'bc:brand', content: this.post.name.replace('&amp;', '&') },
 
         // { property: 'bc:product:category', content: 'Mobile Recharge' },
         { property: 'bc:pop:image', content: image && image.regular },
@@ -111,6 +111,7 @@ export default {
         { property: 'bc:pop:image_banner_mobile', content: banner && banner.mobile },
         { itemprop: 'availability', content: 'http://schema.org/InStock' },
       ].concat(categoryTags),
+      __dangerouslyDisableSanitizers: ['meta'],
       link: [
         { rel: 'canonical', href: url },
       ],
