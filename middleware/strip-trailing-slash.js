adb379d8e6d784083ae6ddfef3d1eabdf2860082
Jeison <51359674+jeisonhiguita@users.noreply.github.com>
2019-06-18 13:30:01 +0200

    Fix/strip trailing slash middleware (#359)
    * fix trailing slash middleware
    
    * fix: add 301 status to redirect

--- middleware/strip-trailing-slash.js
+++ middleware/strip-trailing-slash.js
@@ -1,3 +1,24 @@
+
+function endsWithSlash(url) {
+  return url.slice(-1) === '/';
+}
+
+function stripTrailSlash(url) {
+  if (endsWithSlash(url)) {
+    return stripTrailSlash(url.slice(0, -1));
+  }
+  return url;
+}
+
+
 export default ({ redirect, route }) => {
-  if (route.path.slice(-1) === '/') redirect(301, route.path.slice(0, -1));
+  if (!process.server) return;
+
+  const url = route.path;
+  const times = url.split('/').length - 1;
+
+  if (times > 2 && endsWithSlash(url)) {
+    const formattedUrl = stripTrailSlash(url);
+    redirect(301, formattedUrl);
+  }
 };
