ef6fce13f6947a610f7a97147e4ee069ee28d1f7
Jeison <51359674+jeisonhiguita@users.noreply.github.com>
2019-07-17 09:42:32 +0200

    RAP-2034: Image atom (#405)
    * responsive image atom
    * add viewport addon to storybook
    * add screen breakpoints

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -4,3 +4,4 @@ import '@storybook/addon-actions/register'
 import '@storybook/addon-storysource/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-console';
+import '@storybook/addon-viewport/register';
