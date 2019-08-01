a6dc57077bad2ef05229c0df8375489248a65e64
vpuscoci-cg <valentin.puscoci@cg.nl>
2019-08-01 08:58:55 +0200

    fix: sentry missing link for FAQ

--- client/mixins/faqUrl.js
+++ client/mixins/faqUrl.js
@@ -1,8 +1,10 @@
+import { get } from 'lodash';
+
 export default {
   computed: {
     faqUrl() {
       const { link } = this.$store.getters['shared/customerService'];
-      return link.url || 'https://faq.rapido.com/hc/en-us';
+      return get(link, 'url', 'https://faq.rapido.com/hc/en-us');
     },
   },
 };
