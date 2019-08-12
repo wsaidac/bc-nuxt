e0f995a99361fa33cb8a30e996d19bce176a98b0
Jeison <51359674+jeisonhiguita@users.noreply.github.com>
2019-08-12 12:48:31 +0200

    navbar links molecule (#472)

---
+++ client/components/molecules/header-links/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav>
+    <ul class="flex">
+      <li
+        v-for="(category, index) in items"
+        :key="index"
+        class="mr-6"
+      >
+        <ui-link
+          color="primary"
+          :url="category.url"
+          data-test="header-link"
+        >
+          {{ category.title }}
+        </ui-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import VueTypes from 'vue-types';
+import { UiLink } from '~/components/atoms';
+
+export default {
+  components: {
+    UiLink,
+  },
+  props: {
+    items: VueTypes.arrayOf(
+      VueTypes.shape({
+        url: VueTypes.string.def('#'),
+        title: String,
+      }),
+    ).def([]),
+  },
+};
+
+</script>
