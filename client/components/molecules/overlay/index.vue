9b41a3ae154be56e462f799e52c0cb34f7e4b92b
vpuscoci-cg <valentin.puscoci@cg.nl>
2019-08-20 13:02:05 +0200

    feat: create basic overlay molecule

---
+++ client/components/molecules/overlay/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="state === 'visible'">
+    <section
+      role="dialog"
+      :aria-labelledby="title"
+      :aria-describedby="desc"
+    >
+      <slot @overlay__close:click="onOverlayClose" />
+    </section>
+  </div>
+</template>
+
+<script>
+import VueTypes from 'vue-types';
+
+export default {
+  props: {
+    title: VueTypes.string.def('#'),
+    desc: VueTypes.arrayOf(Object).def([]),
+    state: VueTypes.string.def('hidden'),
+  },
+  methods: {
+    onOverlayClose() {
+      this.state = 'hidden';
+    },
+  },
+};
+</script>
