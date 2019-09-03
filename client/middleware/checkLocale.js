466bb0c1e622c0650096037a44ac787b6ff22d3f
Jeison Higuita <jeison.higuita.sanchez@cg.nl>
2019-09-03 14:48:39 +0200

    chroe: refactor checkLocale middleware

--- client/middleware/checkLocale.js
+++ client/middleware/checkLocale.js
@@ -1,57 +1,90 @@
+const { get } = require('lodash');
+
 const EN_US_LOCALE = 'en-us';
 const COUNTRY_RESTRICTED = 'country-restricted';
 const COUNTRY_RESTRICTED_PATH = `/${COUNTRY_RESTRICTED}`;
 
+const routeDiDNotChange = ({ app, route }) => route.path.substring(1).startsWith(app.i18n.locale);
 
-export default ({
-  app,
-  req,
-  redirect,
-  route,
-}) => {
+const setCountryCookie = (app, countryCode) => {
+  app.$cookies.set('country', countryCode, {
+    path: '/',
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+  });
+};
+
+const getCloudfrontCountryInLocales = (app, cloudfrontViewerCountry) => {
+  const locales = get(app, 'i18n.locales', []);
+
+  const cloudfrontLocale = locales
+    .find(locale => locale.name.toLowerCase() === cloudfrontViewerCountry.toLowerCase());
+
+  return cloudfrontLocale ? cloudfrontLocale.code.toLowerCase() : null;
+};
+
+const redirectIfHasNotCountryCookie = ({ app, req, redirect }) => {
+  const cloudfrontViewerCountry = req ? req.headers['cloudfront-viewer-country'] : null; // format: /pt-pt/
+
+  // if not cloudfront viewer country header redirect to 'country-restricted' page
+  if (!cloudfrontViewerCountry) return redirect(301, COUNTRY_RESTRICTED_PATH);
+  const locale = getCloudfrontCountryInLocales(app, cloudfrontViewerCountry);
+
+  // if the cloudfront-viewer-locale isn't in the i18n current locales redirect to 'country-restricted' page
+  if (!locale) return redirect(301, COUNTRY_RESTRICTED_PATH);
+  // if the extracted cloudfront-viewer-locale code is different to `/en-us` rediret it to `country-restricted` page
+  if (locale !== EN_US_LOCALE) return redirect(301, COUNTRY_RESTRICTED_PATH);
+
+  setCountryCookie(app, locale);
+  return redirect(301, `/${locale}/`);
+};
+
+const redirectIfPathDoesNotHaveLocale = (context) => {
+  const { app, redirect } = context;
+  // check if already exists a cookie with the visitor country
+  const countryCookie = app.$cookies.get('country');
+  if (countryCookie) {
+    return redirect(301, `/${countryCookie}/`);
+  }
+
+  return redirectIfHasNotCountryCookie(context);
+};
+
+
+const redirectIfLocaleIsRestricted = (context) => {
+  const { app, redirect } = context;
+  // check if already exists a cookie with the visitor country
+  const countryCookie = app.$cookies.get('country');
+  if (countryCookie) {
+    if (countryCookie !== EN_US_LOCALE) {
+      return redirect(301, COUNTRY_RESTRICTED_PATH);
+    }
+    return redirect(301, `/${countryCookie}/`);
+  }
+
+  return redirectIfHasNotCountryCookie(context);
+};
+
+export default (context = {}) => {
+  const {
+    app,
+    redirect,
+    route,
+  } = context;
   // if the locale hasn't change
-  if (!process.server && route.path.substring(1).startsWith(app.i18n.locale)) return null;
+  if (!process.server && routeDiDNotChange(context)) return null;
 
-  const urlPaths = route.path.split('/');
-  const currentLocale = urlPaths[1];
+  const urlPaths = route.path.split('/').filter(Boolean); // ['locale', 'category']
+  const basePath = urlPaths[0];
 
-
-  if (currentLocale === COUNTRY_RESTRICTED) {
-    return redirect('/country-restricted');
+  // if basePath === 'country-restricted' continue
+  if (basePath === COUNTRY_RESTRICTED) {
+    return null;
   }
 
-  // if there is no locale slug in the url, redirect to default locale
-  if (!currentLocale || currentLocale === EN_US_LOCALE) {
-    const countryCookie = app.$cookies.get('country');
-    if (countryCookie) {
-      return redirect(301, `/${countryCookie}/`);
-    }
+  const currentLocale = basePath;
+  if (!currentLocale) return redirectIfPathDoesNotHaveLocale(context);
 
-    const cloudfrontViewerCountry = req ? req.headers['cloudfront-viewer-country'] : null;
-
-    if (cloudfrontViewerCountry) {
-      const cloudfrontLocale = app.i18n.locales
-        .find(locale => locale.name.toLowerCase() === cloudfrontViewerCountry.toLowerCase());
-
-      const localeCode = cloudfrontLocale && cloudfrontLocale.code;
-
-
-      if (localeCode) {
-        if (localeCode.toLowerCase() !== EN_US_LOCALE && currentLocale.toLowerCase() === EN_US_LOCALE) {
-          return redirect(301, COUNTRY_RESTRICTED_PATH);
-        }
-
-        app.$cookies.set('country', localeCode, {
-          path: '/',
-          maxAge: 60 * 60 * 24 * 7, // 7 days
-        });
-
-        return redirect(301, `/${localeCode}/`);
-      }
-    }
-
-    return redirect(301, COUNTRY_RESTRICTED_PATH);
-  }
+  if (currentLocale === EN_US_LOCALE) return redirectIfLocaleIsRestricted(context);
 
   // if the locale has uppercase letters, redirect to lowercase locale
   // ex /en-US/ will redirect to /en-us/ for SEO purposes
@@ -62,10 +95,7 @@ export default ({
   }
 
   if (currentLocale) {
-    app.$cookies.set('country', currentLocale, {
-      path: '/',
-      maxAge: 60 * 60 * 24 * 7, // 7 days
-    });
+    setCountryCookie(app, currentLocale);
   }
 
   return null;
