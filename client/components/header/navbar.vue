6e5445fb5419d50bcdb7a8ce2bda7201aba3ad39
Jeison <51359674+jeisonhiguita@users.noreply.github.com>
2019-09-09 13:04:12 +0200

    fix: Add the correct locale to logo link (#535)

--- client/components/header/navbar.vue
+++ client/components/header/navbar.vue
@@ -111,7 +111,14 @@ export default {
       return this.$i18n.locales.find(i => i.code === this.$i18n.locale);
     },
     homeLink() {
-      return this.$route.path.slice(0, 7);
+      const path = this.$route.path.slice(1, 6);
+      const locale = this.$i18n.locales.find(i => i.code === path);
+
+      if (locale) {
+        return `/${locale.code}/`;
+      }
+
+      return `/${this.$i18n.locale}/`;
     },
   },
 };
