f5c424b911d68a2f4f739f3560c44696b907db2d
ronald <ronald@10kb.nl>
2019-09-20 09:51:28 +0200

    vuex test for index-store

--- client/store/index/index-store.test.js
+++ client/store/index/index-store.test.js
@@ -1,18 +1,69 @@
+import uuid from 'uuid/v4';
 import store from './index';
 import storeContext from '~/test/utils/with-store-context';
 
+jest.mock('uuid/v4');
+uuid.mockReturnValueOnce(1);
+
+const { app, error } = storeContext;
+const query = { aid: 'aid' };
+
 const { getters, actions } = store;
 
-describe('store: index', () => {
-  it('actions: `nuxtServerInit`', () => {
-    const commit = jest.fn();
-    const dispatch = jest.fn();
+const commit = jest.fn();
+const dispatch = jest.fn();
+
+describe('store: index, actions', () => {
+  beforeEach(() => {
     actions.nuxtServerInit({ commit, dispatch }, {
-      query: { aid: 'aid' },
       ...storeContext,
+      query,
       req: { headers: { host: null } },
     });
+  });
 
-    expect(commit).toHaveBeenCalledTimes(1);
+  /**
+   * Actions
+   */
+  it('actions: `nuxtServerInit` - should commit `context/setDomain`', () => {
+    expect(commit).toHaveBeenCalledWith('context/setDomain', null);
+  });
+
+  it('actions: `nuxtServerInit` - should dispatch `context/changeContext`', () => {
+    expect(dispatch).toHaveBeenCalledWith('context/changeContext', { app, error });
+  });
+
+  it('actions: `nuxtServerInit`- should call app.$cookies.set with aid', () => {
+    expect(app.$cookies.set).toHaveBeenCalledWith('aid', query.aid, { path: '/' });
+  });
+
+  it('actions: `nuxtServerInit`- should call app.$cookies.set with RiskifiedSessionId', () => {
+    expect(app.$cookies.set).toHaveBeenCalledWith(
+      'RiskifiedSessionId',
+      uuid(),
+      { path: '/' },
+    );
+  });
+});
+
+
+describe('store: index, getters', () => {
+  /**
+   * Getters
+   */
+  it('getters: `extendedGraphqlHeaders`', () => {
+    const rootGetters = {
+      'auth/token': 'mockToken',
+      'context/cmsContext': 'mockContext',
+      'context/labelId': 'mockLabelId',
+    };
+    const rootState = { i18n: { locale: 'nl-nl' } };
+    const result = getters.extendedGraphqlHeaders(null, null, rootState, rootGetters);
+    expect(result).toEqual({
+      'Authorization': 'Bearer mockToken',
+      'X-Cms-Context': 'mockContext',
+      'X-Authorization-Context': 'mockLabelId',
+      'X-Locale-Context': 'nl-nl',
+    });
   });
 });
