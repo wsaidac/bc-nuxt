f7eb5e5ef23bb211fe4e02d5190c1c8899879ed9
ronald <ronald@10kb.nl>
2019-09-26 10:58:05 +0200

    adjust handleTrailingSlash

--- client/middleware/handleTrailingSlash.js
+++ client/middleware/handleTrailingSlash.js
@@ -1,34 +1,20 @@
-
-function endsWithSlash(url) {
-  return url.slice(-1) === '/';
-}
-
-function stripTrailSlash(url) {
-  if (endsWithSlash(url)) {
-    return stripTrailSlash(url.slice(0, -1));
-  }
-  return url;
-}
-
-function isValidLocale(slug) {
-  return slug.length === 5 && slug[2] === '-';
-}
-
 export default ({ redirect, route }) => {
   if (!process.server) return;
 
   const url = route.path; // example: /en-us/xbox-giftcard/
-
   const urlBreadcrumbs = url.split('/').filter(Boolean); // ['en-us', 'xbox-giftcard']
-  const times = urlBreadcrumbs.length;
-  const locale = urlBreadcrumbs[0]; // /en-us/ or /country-restricted
+  const localeOnly = urlBreadcrumbs.length === 1;
+  const isValidLocale = /^\w\w-\w\w$/.test(urlBreadcrumbs[0]); // /en-us/ or /country-restricted ---> true
 
-  if (times === 1 && isValidLocale(locale) && !endsWithSlash(url)) {
-    redirect(301, `${url}/`);
+  // /nl-nl AND /nl-nl//// ---> /nl-nl/
+  const endsNotWithOneSlash = /.*?\w\/$/.test(url);
+  if (localeOnly && isValidLocale && !endsNotWithOneSlash) {
+    redirect(301, `/${urlBreadcrumbs}/`);
   }
 
-  if (times > 1 && endsWithSlash(url)) {
-    const formattedUrl = stripTrailSlash(url);
-    redirect(301, formattedUrl);
+  // /nl-nl/xbox-giftcard/ AND /nl-nl/xbox-giftcard//// ---> /nl-nl/xbox-giftcard
+  const endsWithSlash = /.*?\/$/.test(url);
+  if (!localeOnly && isValidLocale && endsWithSlash) {
+    redirect(301, url.replace(/\/+$/, ''));
   }
 };
