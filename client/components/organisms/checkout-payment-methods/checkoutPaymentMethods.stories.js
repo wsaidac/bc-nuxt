a70e7ef565d964aeb90496ff802b5353a3d148bb
Jeison <51359674+jeisonhiguita@users.noreply.github.com>
2019-09-30 15:15:56 +0200

    feat: checkout payment methods organism (#574)
    * feat: checkout payment methods organism

---
+++ client/components/organisms/checkout-payment-methods/checkoutPaymentMethods.stories.js
@@ -0,0 +1,99 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import Vue from 'vue';
+import { storiesOf } from '@storybook/vue';
+import CheckoutPaymentMethods from './index.vue';
+
+
+// definition
+Vue.component('checkout-payment-methods', CheckoutPaymentMethods);
+
+const paymentMethods = [
+  {
+    id: 1,
+    name: 'Visa',
+    image: 'https://cdn.zeplin.io/5d23163f91be672e1f32200e/assets/E1DFEF2D-D719-4A96-A7BC-CBB007FFC17A.svg',
+    serviceFee: 'Free',
+  },
+  {
+    id: 2,
+    name: 'PayPal',
+    image: 'https://cdn.zeplin.io/5d23163f91be672e1f32200e/assets/36DBBF0F-884C-494D-B84B-00C2B2D3B277.svg',
+    serviceFee: 'Free',
+  },
+  {
+    id: 3,
+    name: 'IDeal',
+    image: 'https://cdn.zeplin.io/5d23163f91be672e1f32200e/assets/E7451771-2539-41A6-B3AB-2D162B68039B.svg',
+    serviceFee: 'Free',
+  },
+  {
+    id: 4,
+    name: 'Master Card',
+    image: 'https://cdn.zeplin.io/5d23163f91be672e1f32200e/assets/F611E13F-D4BC-43AD-8822-5F836C7133C2.svg',
+    serviceFee: 'Free',
+  },
+  {
+    id: 5,
+    name: 'American Express',
+    image: 'https://cdn.zeplin.io/5d23163f91be672e1f32200e/assets/010DF931-82D8-422E-8E4B-BCB09AE1C1C3.svg',
+    serviceFee: 'Free',
+  },
+];
+// stories
+storiesOf('Organisms/Checkout Payment Methods', module)
+  .add('default', () => ({
+    data() {
+      return {
+        paymentMethods,
+      };
+    },
+    template: `
+      <container class="py-6">
+        <row>
+          <column class="w-full lg:w-7/12">
+            <checkout-payment-methods :payment-methods="paymentMethods"/>
+          </column>
+        </row>
+      </container>
+      `,
+    components: { CheckoutPaymentMethods },
+  }))
+  .add(':disable="true"', () => ({
+    data() {
+      return {
+        paymentMethods,
+      };
+    },
+    template: `
+      <container class="py-6">
+        <row>
+          <column class="w-full lg:w-7/12">
+            <checkout-payment-methods
+              :payment-methods="paymentMethods"
+              disable
+            />
+          </column>
+        </row>
+      </container>
+      `,
+    components: { CheckoutPaymentMethods },
+  }))
+  .add('without payment methods', () => ({
+    data() {
+      return {
+        paymentMethods,
+      };
+    },
+    template: `
+      <container class="py-6">
+        <row>
+          <column class="w-full lg:w-7/12">
+            <checkout-payment-methods
+              :payment-methods="[]"
+            />
+          </column>
+        </row>
+      </container>
+      `,
+    components: { CheckoutPaymentMethods },
+  }));
---
+++ client/components/organisms/checkout-payment-methods/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <card
+    class="relative"
+    :class="[{'opacity-25': disable }]"
+  >
+    <div
+      v-if="disable"
+      class="inset-0 absolute"
+    />
+    <ui-title type="h3">{{ $t("pages.checkout.payment_methods.title") }}</ui-title>
+    <p class="py-4">{{ $t("pages.checkout.payment_methods.description") }}</p>
+    <payment-methods-checklist
+      v-if="paymentMethods.length"
+      :payment-methods="paymentMethods"
+    />
+    <div
+      v-else
+      class="border border-gray p-4"
+    >
+      TODO: Empty state :)
+    </div>
+  </card>
+</template>
+
+<script>
+import VueTypes from 'vue-types';
+
+import { UiTitle, Card } from '~/components/atoms';
+import { PaymentMethodsChecklist } from '~/components/molecules';
+
+export default {
+  components: {
+    UiTitle,
+    Card,
+    PaymentMethodsChecklist,
+  },
+  props: {
+    paymentMethods: VueTypes.array.def([]),
+    disable: Boolean,
+  },
+};
+
+</script>
